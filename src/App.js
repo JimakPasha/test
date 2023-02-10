fc3be4db38270e19752ffc0e05ad4c75ada363bf
pavel_hladki <pavel_hladki@senla.eu>
2023-02-10 18:06:13 +0300

    feat: add blocks, timer, animation

--- src/App.js
+++ src/App.js
@@ -1,23 +1,33 @@
-import logo from './logo.svg';
+import { useState, useEffect } from 'react';
 import './App.css';
 
 function App() {
+  const [seconds, setSeconds] = useState(5);
+  const [timerActive, setTimerActive] = useState(false);
+
+  useEffect(() => {
+    if (seconds > 0 && timerActive) {
+      setTimeout(setSeconds, 1000, seconds - 1);
+    } else {
+      setTimerActive(false);
+      setSeconds(5);
+    }
+  }, [seconds, timerActive]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="field">
+        <div className="block action-square">1</div>
+        <div className="ball action-ball"></div>
+        <div className="block">2</div>
+      </div>
+      <button
+        className="btn"
+        onClick={() => setTimerActive(!timerActive)}
+        disabled={timerActive}
+      >
+        {timerActive ? seconds : 'start'}
+      </button>
     </div>
   );
 }
